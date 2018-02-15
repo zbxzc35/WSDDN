9d05c291dcdc6ae1289afd8c9ac644e4b5781f39
ptx9363 <ptx9363@gmail.com>
2018-02-16 00:54:27 +0800

    first commit

---
+++ datasets/generate_pascalvoc_tfrecords.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# Copyright 2015 Paul Balanca. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+''' Generate tfrecords for PASCAL VOC datasets
+Modification from https://github.com/balancap/SSD-Tensorflow/blob/master/datasets/pascalvoc_to_tfrecords.py
+
+Each record contains following fields:
+
+    image/encoded: raw image encoded in RGB
+    image/height, image/width, image/channel: height, width, channel
+    image/object/bbox/xmin, xmax, ymin, ymax: list of bboxes coords
+    image/object/label: list of integer specifying the classification label for each box
+
+'''
+import os
+import sys
+import random
+
+import tensorflow as tf
+
+import xml.etree.ElementTree as ET
+
+from dataset_utils import int64_feature, float_feature, bytes_feature
+
+# VOC labels
+VOC_LABELS = {
+    'none': (0, 'Background'),
+    'aeroplane': (1, 'Vehicle'),
+    'bicycle': (2, 'Vehicle'),
+    'bird': (3, 'Animal'),
+    'boat': (4, 'Vehicle'),
+    'bottle': (5, 'Indoor'),
+    'bus': (6, 'Vehicle'),
+    'car': (7, 'Vehicle'),
+    'cat': (8, 'Animal'),
+    'chair': (9, 'Indoor'),
+    'cow': (10, 'Animal'),
+    'diningtable': (11, 'Indoor'),
+    'dog': (12, 'Animal'),
+    'horse': (13, 'Animal'),
+    'motorbike': (14, 'Vehicle'),
+    'person': (15, 'Person'),
+    'pottedplant': (16, 'Indoor'),
+    'sheep': (17, 'Animal'),
+    'sofa': (18, 'Indoor'),
+    'train': (19, 'Vehicle'),
+    'tvmonitor': (20, 'Indoor'),
+}
+
+# Original dataset organisation.
+DIRECTORY_ANNOTATIONS = 'Annotations/'
+DIRECTORY_IMAGES = 'JPEGImages/'
+
+# TFRecords convertion parameters.
+RANDOM_SEED = 4242
+SAMPLES_PER_FILES = 200
+
+def _process_image(directory, name):
+    '''
+    '''
+
+    # Read the image file.
+    filename = directory + DIRECTORY_IMAGES + name + '.jpg'
+    image_data = tf.gfile.FastGFile(filename, 'rb').read()
+
+    # Read the XML annotation file.
+    filename = os.path.join(directory, DIRECTORY_ANNOTATIONS, name + '.xml')
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    # Image shape.
+    size = root.find('size')
+    shape = [int(size.find('height').text),
+             int(size.find('width').text),
+             int(size.find('depth').text)]
+
+    # Find annotations.
+    bboxes = []
+    labels = []
+    difficult = []
+    truncated = []
+    for obj in root.findall('object'):
+        label = obj.find('name').text
+        labels.append(int(VOC_LABELS[label][0]))
+
+        if obj.find('difficult'):
+            difficult.append(int(obj.find('difficult').text))
+        else:
+            difficult.append(0)
+        if obj.find('truncated'):
+            truncated.append(int(obj.find('truncated').text))
+        else:
+            truncated.append(0)
+
+        bbox = obj.find('bndbox')
+        bboxes.append((float(bbox.find('ymin').text) / shape[0],
+                       float(bbox.find('xmin').text) / shape[1],
+                       float(bbox.find('ymax').text) / shape[0],
+                       float(bbox.find('xmax').text) / shape[1]
+                       ))
+
+    return image_data, shape, bboxes, labels, difficult, truncated
+
+def  _convert_to_example(image_data, shape, bboxes, labels,
+                        difficult, truncated):
+    '''
+    '''
+    xmin = []
+    ymin = []
+    xmax = []
+    ymax = []
+    for b in bboxes:
+        assert len(b) == 4
+        # pylint: disable=expression-not-assigned
+        [l.append(point) for l, point in zip([ymin, xmin, ymax, xmax], b)]
+        # pylint: enable=expression-not-assigned
+
+    image_format = b'JPEG'
+    example = tf.train.Example(features=tf.train.Features(feature={
+            'image/height': int64_feature(shape[0]),
+            'image/width': int64_feature(shape[1]),
+            'image/channels': int64_feature(shape[2]),
+            'image/shape': int64_feature(shape),
+            'image/object/bbox/xmin': float_feature(xmin),
+            'image/object/bbox/xmax': float_feature(xmax),
+            'image/object/bbox/ymin': float_feature(ymin),
+            'image/object/bbox/ymax': float_feature(ymax),
+            'image/object/bbox/label': int64_feature(labels),
+            'image/object/bbox/difficult': int64_feature(difficult),
+            'image/object/bbox/truncated': int64_feature(truncated),
+            'image/format': bytes_feature(image_format),
+            'image/encoded': bytes_feature(image_data)}))
+    return example
+
+def _add_to_tfrecord(dataset_dir, name, tfrecord_writer):
+    """Loads data from image and annotations files and add them to a TFRecord.
+    Args:
+      dataset_dir: Dataset directory;
+      name: Image name to add to the TFRecord;
+      tfrecord_writer: The TFRecord writer to use for writing.
+    """
+    image_data, shape, bboxes, labels, difficult, truncated = \
+        _process_image(dataset_dir, name)
+    example = _convert_to_example(image_data, shape, bboxes, labels, difficult, truncated)
+    tfrecord_writer.write(example.SerializeToString())
+
+def _get_output_filename(output_dir, name, idx):
+    return '%s/%s_%03d.tfrecord' % (output_dir, name, idx)
+
+def run(dataset_dir, output_dir, name='voc_train', shuffling=False):
+    """Runs the conversion operation.
+    Args:
+      dataset_dir: The dataset directory where the dataset is stored.
+      output_dir: Output directory.
+    """
+    if not tf.gfile.Exists(dataset_dir):
+        tf.gfile.MakeDirs(dataset_dir)
+
+    # Dataset filenames, and shuffling.
+    path = os.path.join(dataset_dir, DIRECTORY_ANNOTATIONS)
+    filenames = sorted(os.listdir(path))
+    if shuffling:
+        random.seed(RANDOM_SEED)
+        random.shuffle(filenames)
+
+    # Process dataset files.
+    i = 0
+    fidx = 0
+    while i < len(filenames):
+        # Open new TFRecord file.
+        tf_filename = _get_output_filename(output_dir, name, fidx)
+        with tf.python_io.TFRecordWriter(tf_filename) as tfrecord_writer:
+            j = 0
+            while i < len(filenames) and j < SAMPLES_PER_FILES:
+                sys.stdout.write('\r>> Converting image %d/%d' % (i+1, len(filenames)))
+                sys.stdout.flush()
+
+                filename = filenames[i]
+                img_name = filename[:-4]
+                _add_to_tfrecord(dataset_dir, img_name, tfrecord_writer)
+                i += 1
+                j += 1
+            fidx += 1
+
+    # Finally, write the labels file:
+    print('\nFinished converting the Pascal VOC dataset!')
+
+if __name__ =='__main__':
+    '''
+    usage: ./generate_pascalvoc_tfrecords.py /path/to/VOCdevkit/VOC2007 /path/to/output/
+    '''
+    DATASET_DIR = sys.argv[1]
+    OUTPUT_DIR = sys.argv[2]
+    run(DATASET_DIR, OUTPUT_DIR)
---
+++ nets/wsddn.py
@@ -0,0 +1,92 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+"""
+"""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import tensorflow as tf
+
+slim = tf.contrib.slim
+
+
+def vgg_arg_scope(weight_decay=0.0005):
+  """Defines the VGG arg scope.
+
+  Args:
+    weight_decay: The l2 regularization coefficient.
+
+  Returns:
+    An arg_scope.
+  """
+  with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                      activation_fn=tf.nn.relu,
+                      weights_regularizer=slim.l2_regularizer(weight_decay),
+                      biases_initializer=tf.zeros_initializer()):
+    with slim.arg_scope([slim.conv2d], padding='SAME') as arg_sc:
+      return arg_sc
+
+
+def vgg_16(inputs,
+           num_classes=1000,
+           is_training=True,
+           dropout_keep_prob=0.5,
+           spatial_squeeze=True,
+           scope='vgg_16',
+           fc_conv_padding='VALID',
+           global_pool=False):
+  """Oxford Net VGG 16-Layers version D Example.
+
+  """
+  with tf.variable_scope(scope, 'vgg_16', [inputs]) as sc:
+    end_points_collection = sc.original_name_scope + '_end_points'
+    # Collect outputs for conv2d, fully_connected and max_pool2d.
+    with slim.arg_scope([slim.conv2d, slim.fully_connected, slim.max_pool2d],
+                        outputs_collections=end_points_collection):
+      net = slim.repeat(inputs, 2, slim.conv2d, 64, [3, 3], scope='conv1')
+      net = slim.max_pool2d(net, [2, 2], scope='pool1')
+      net = slim.repeat(net, 2, slim.conv2d, 128, [3, 3], scope='conv2')
+      net = slim.max_pool2d(net, [2, 2], scope='pool2')
+      net = slim.repeat(net, 3, slim.conv2d, 256, [3, 3], scope='conv3')
+      net = slim.max_pool2d(net, [2, 2], scope='pool3')
+      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv4')
+      net = slim.max_pool2d(net, [2, 2], scope='pool4')
+      net = slim.repeat(net, 3, slim.conv2d, 512, [3, 3], scope='conv5')
+      net = slim.max_pool2d(net, [2, 2], scope='pool5')
+
+      # Use conv2d instead of fully_connected layers.
+      net = slim.conv2d(net, 4096, [7, 7], padding=fc_conv_padding, scope='fc6')
+      net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                         scope='dropout6')
+      net = slim.conv2d(net, 4096, [1, 1], scope='fc7')
+      # Convert end_points_collection into a end_point dict.
+      end_points = slim.utils.convert_collection_to_dict(end_points_collection)
+      if global_pool:
+        net = tf.reduce_mean(net, [1, 2], keep_dims=True, name='global_pool')
+        end_points['global_pool'] = net
+      if num_classes:
+        net = slim.dropout(net, dropout_keep_prob, is_training=is_training,
+                           scope='dropout7')
+        net = slim.conv2d(net, num_classes, [1, 1],
+                          activation_fn=None,
+                          normalizer_fn=None,
+                          scope='fc8')
+        if spatial_squeeze and num_classes is not None:
+          net = tf.squeeze(net, [1, 2], name='fc8/squeezed')
+        end_points[sc.name + '/fc8'] = net
+      return net, end_points
+vgg_16.default_image_size = 224
+
